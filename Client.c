22686530337be05c22f6368dd2b1fd9b010050f4
EyobelB <eyobel.berhane@gmail.com>
2020-07-31 21:40:39 -0500

    Pwd functionality
    Password can be sent from client, verified on the server, and client can be accepted based on correct password.

--- Client.c
+++ Client.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 #include <unistd.h>
+
+#define MAX_ARR_LEN 256
 
 
 
@@ -12,8 +15,8 @@
 int main()
 {
     //Create a socket
-    int socketData;
-    socketData = socket(AF_INET, SOCK_STREAM, 0);
+    int clientSocket;
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     //Specify an address for the client
     struct sockaddr_in server_address;
@@ -21,31 +24,41 @@ int main()
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    int connection_status = connect(socketData, (struct sockaddr *) &server_address, sizeof(server_address));
+    int connection_status = connect(clientSocket, (struct sockaddr *) &server_address, sizeof(server_address));
 
     //Check for potential connection errors
-    if(connection_status == -1)
+    if (connection_status == -1)
     {
         printf("Server: There was an error in making a connection to the remote socket.");
+        close(clientSocket);
     }
 
-    char message[256];
-    printf(": ");
-    fgets(message, 256, stdin);
-    
-
-    send(socketData, message, sizeof(message), 0);
-
-    
     //This code will receive(recv) data from a server
-    //char server_output[256]; //String that holds response from server
-    //recv(socketData, &server_output, sizeof(server_output), 0);
+    char server_output[MAX_ARR_LEN];
+    char clientPasscode[MAX_ARR_LEN];
+    char pwdMessage[MAX_ARR_LEN];
+
+    recv(clientSocket, &server_output, sizeof(server_output), 0);
+    printf("Server: %s\n", server_output);
+    recv(clientSocket, &server_output, MAX_ARR_LEN, 0);
+
+    char pwdRequest[256] = "Enter a password below";
 
     //print out data that has been received from the server
-    //printf("Server: %s", server_output);
-    
+    printf("Server: %s\n", pwdRequest);
+    scanf("%s", clientPasscode);
+    //fgets(clientPasscode, MAX_ARR_LEN, stdin);
 
-    //finally, close the socket.
-    close(socketData);
+
+    //Send the client passcode to the server via a loop, one character at a time
+
+    send(clientSocket, clientPasscode, MAX_ARR_LEN, 0);
+
+    //Allow for message to be received from server as to whether or not kick has happened
+    recv(clientSocket, &pwdMessage, MAX_ARR_LEN, 0);
+    printf("%s", pwdMessage);
+
+    close(clientSocket);
+
     return 0;
 }
--- Server.c
+++ Server.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 #include <unistd.h>
 
+#define MAX_ARR_LEN 256
+
 
 int main()
 {
-    char server_message[256] = "Successfully connected!";
+    char server_message[MAX_ARR_LEN] = "Successfully connected!";
+
 
     int serverSocket;
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -21,6 +25,8 @@ int main()
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
+
+
     //bind the socket to the specified IP and port
     bind(serverSocket, (struct sockaddr*) &server_address, sizeof(server_address));
 
@@ -29,19 +35,35 @@ int main()
 
     //Start accepting client connections
     int client_socket;
+    char passcode[MAX_ARR_LEN] = "yemane12";
+    char clientPasscode[MAX_ARR_LEN];
+    int isKicked;
+
+
+    //Accept for now
     client_socket = accept(serverSocket, NULL, NULL);
-   
 
-    //Send a server message
-    //send(client_socket, server_message, sizeof(server_message), 0);
-    
-    char message_output[256];
-    recv(client_socket, &message_output, sizeof(message_output), 0);
-    printf("%d: %s", client_socket, message_output);
+    send(client_socket, server_message, sizeof(server_message), 0);
+    send(client_socket, "Enter the passcode below", MAX_ARR_LEN, 0);
 
-    //close the socket now
+    //Receive the client passcode, one byte at a time
+    recv(client_socket, &clientPasscode, MAX_ARR_LEN, 0);
+
+
+    //Check for password compliance
+    if(strcmp(passcode, clientPasscode) != 0)
+    {
+        send(client_socket, "Sorry, wrong password...", MAX_ARR_LEN, 0);
+    }
+    else
+    {
+        send(client_socket, "Welcome to the server!", MAX_ARR_LEN, 0);
+    }
+
+
     close(serverSocket);
+
+
 
     return 0;
 }
-
