cc2d4554fc9041192025ad50bd3fe14f2f731bdb
EyobelB <eyobel.berhane@gmail.com>
2020-08-02 22:43:26 -0500

    Update Client.c

--- Client.c
+++ Client.c
@@ -35,24 +35,36 @@ int main()
 
     //This code will receive(recv) data from a server
     char server_output[MAX_ARR_LEN];
-    char clientPasscode[MAX_ARR_LEN];
+    char* clientPasscode = calloc(MAX_ARR_LEN, sizeof(char));
     char pwdMessage[MAX_ARR_LEN];
 
     recv(clientSocket, &server_output, sizeof(server_output), 0);
     printf("Server: %s\n", server_output);
     recv(clientSocket, &server_output, MAX_ARR_LEN, 0);
 
-    char pwdRequest[256] = "Enter a password below";
+    char pwdRequest[MAX_ARR_LEN] = "Enter a password below";
 
     //print out data that has been received from the server
     printf("Server: %s\n", pwdRequest);
     scanf("%s", clientPasscode);
-    //fgets(clientPasscode, MAX_ARR_LEN, stdin);
+
+    //Optimize the memory allocation by removing everything after the new line terminator
+    int stringSize = 0;
+    for(int i = 0; i < MAX_ARR_LEN; i++)
+    {
+        if(clientPasscode[i] != '\n')
+        {
+            continue;
+        }
+        else
+        {
+            clientPasscode = (char*) realloc(clientPasscode, i + 1);
+        }
+    }
 
 
-    //Send the client passcode to the server via a loop, one character at a time
-
-    send(clientSocket, clientPasscode, MAX_ARR_LEN, 0);
+    //Send the client passcode to the server
+    send(clientSocket, clientPasscode, sizeof(clientPasscode), 0);
 
     //Allow for message to be received from server as to whether or not kick has happened
     recv(clientSocket, &pwdMessage, MAX_ARR_LEN, 0);
