758d9d0acfaa8678cb41316234137dd991b1b521
Nathanael Berhane <natuyemane04@gmail.com>
2020-08-06 12:26:20 -0500

    messages from both users
    multiple users can send messages that are received by the server and printed to the console

--- Client.c
+++ Client.c
@@ -3,8 +3,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <string.h>
 #include <unistd.h>
+
+#define MAX_ARR_LEN 256
 
 
 
@@ -12,40 +14,40 @@
 int main()
 {
     //Create a socket
-    int socketData;
-    socketData = socket(AF_INET, SOCK_STREAM, 0);
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     //Specify an address for the client
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
+    server_address.sin_port = htons(6667);
     server_address.sin_addr.s_addr = INADDR_ANY;
-
-    int connection_status = connect(socketData, (struct sockaddr *) &server_address, sizeof(server_address));
-
-    //Check for potential connection errors
-    if(connection_status == -1)
+    int connection_status;
+    while((connection_status = connect(server_socket, (struct sockaddr *) &server_address, sizeof(server_address))))
     {
-        printf("Server: There was an error in making a connection to the remote socket.");
+        printf("Tring to connect...\n");
     }
 
-    char message[256];
-    printf(": ");
-    fgets(message, 256, stdin);
+    int exit = 0;
+    char username[MAX_ARR_LEN] = {};
+    while(exit == 0) 
+    {
+        char messageToSend[MAX_ARR_LEN];
+        printf("%s: ", username);
+        fgets(messageToSend, MAX_ARR_LEN, stdin);
+        strtok(messageToSend, "\n");
+        if(strcmp("~", messageToSend) == 0)
+        {
+            return 0;
+        }
+        send(server_socket, messageToSend, MAX_ARR_LEN, 0);
+
+    }
     
 
-    send(socketData, message, sizeof(message), 0);
-
     
-    //This code will receive(recv) data from a server
-    //char server_output[256]; //String that holds response from server
-    //recv(socketData, &server_output, sizeof(server_output), 0);
-
-    //print out data that has been received from the server
-    //printf("Server: %s", server_output);
     
 
-    //finally, close the socket.
-    close(socketData);
+    close(server_socket);
     return 0;
 }
--- Server.c
+++ Server.c
@@ -3,13 +3,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <string.h>
 #include <unistd.h>
+#define MAX_ARR_LEN 256
 
 
 int main()
 {
-    char server_message[256] = "Successfully connected!";
+    char server_message[MAX_ARR_LEN] = "Successfully connected!";
 
     int serverSocket;
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,26 +19,30 @@ int main()
     //define the server address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
+    server_address.sin_port = htons(6667);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     //bind the socket to the specified IP and port
     bind(serverSocket, (struct sockaddr*) &server_address, sizeof(server_address));
 
     //Second parameter on listen determines how many clients to listen for
-    listen(serverSocket, 1);
+    listen(serverSocket, 5);
 
     //Start accepting client connections
     int client_socket;
-    client_socket = accept(serverSocket, NULL, NULL);
-   
+    while((client_socket = accept(serverSocket, (struct sockaddr *)NULL, NULL))) 
+    {
+        pid_t pid;
+        char message[MAX_ARR_LEN] = "";
+        if((pid = fork()) == 0) 
+        {
+            while (recv(client_socket, &message, MAX_ARR_LEN, 0)>0) 
+            {
+                printf("%d: %s\n", client_socket, message);
+            }
+        }
+}
 
-    //Send a server message
-    //send(client_socket, server_message, sizeof(server_message), 0);
-    
-    char message_output[256];
-    recv(client_socket, &message_output, sizeof(message_output), 0);
-    printf("%d: %s", client_socket, message_output);
 
     //close the socket now
     close(serverSocket);
